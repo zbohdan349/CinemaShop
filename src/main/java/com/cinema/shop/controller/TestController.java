923692e36dd9e9d58a750a9f1f772539077f7575
Bobo <zbogdan349@gmail.com>
2022-06-12 17:57:53 +0300

    add @CrossOrigin for TestController.java

--- src/main/java/com/cinema/shop/controller/TestController.java
+++ src/main/java/com/cinema/shop/controller/TestController.java
@@ -3,12 +3,14 @@ package com.cinema.shop.controller;
 import com.cinema.shop.model.Film;
 import com.cinema.shop.repository.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class TestController {
 
