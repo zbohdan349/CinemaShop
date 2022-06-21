94083c211d98f659d953837998e60b7befc0b731
zbohdan <zbogdan349@gmail.com>
2022-06-21 16:31:02 +0300

    add Category to Film

---
+++ src/main/java/com/cinema/shop/model/Category.java
@@ -0,0 +1,54 @@
+package com.cinema.shop.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Integer id;
+
+    private String name;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    private Set<Film> films;
+
+
+    public Category() {
+    }
+
+    public Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(Set<Film> films) {
+        this.films = films;
+    }
+}
--- src/main/java/com/cinema/shop/model/Film.java
+++ src/main/java/com/cinema/shop/model/Film.java
@@ -5,6 +5,7 @@ package com.cinema.shop.model;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.Year;
+import java.util.Set;
 
 
 @Entity
@@ -30,6 +31,14 @@ public class Film {
 
     @Column(name = "replacement_cost")
     private BigDecimal buyRate;
+
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+            name = "film_category",
+            joinColumns = { @JoinColumn(name = "film_id") },
+            inverseJoinColumns = { @JoinColumn(name = "category_id") }
+    )
+    private Set<Category> categories;
 
 
     public Film() {
@@ -95,6 +104,14 @@ public class Film {
 
     public void setBuyRate(BigDecimal buyRate) {
         this.buyRate = buyRate;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 
     @Override
